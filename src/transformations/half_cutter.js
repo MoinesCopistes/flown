67281bfe228d5dec104658773e964021eb0cca96
kiwi <kiwi.bp.surviv@gmail.com>
2024-11-23 09:56:36 +0100

    setting mask to 255

--- src/transformations/half_cutter.js
+++ src/transformations/half_cutter.js
@@ -1,11 +1,10 @@
 import { get_base } from "../utils"
 
 export function half_cutter(image, p) {
-    let base = get_base(p);
-    base.clear()
-    base.fill(0);
-    base.rect(base.width / 2, base.height / 2, -base.width / 2, -base.height)
-    let mask = base.get()
-    image.mask(mask)
-    return image
+    let base = p.createGraphics(p.width, p.height);
+    base.image(image, 0, 0)
+    base.fill(255);
+    base.noStroke();
+    base.rect(0, 0, base.width / 2, base.height)
+    return base.get()
 }
