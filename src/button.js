ea8a62546edc4ba5fd1dd8afbf429cea4ce571c0
kiwi <kiwi.bp.surviv@gmail.com>
2024-11-23 16:48:28 +0100

    animations

--- src/button.js
+++ src/button.js
@@ -1,5 +1,6 @@
-export function addButton(container, color, callback) {
-
+export function addButton(container, color, callback, animation = null) {
+    console.log("adding button")
+    console.log(color)
     let btn = document.createElement("button")
     btn.style.setProperty("--color", color);
     btn.className = "transformation"
@@ -9,6 +10,18 @@ export function addButton(container, color, callback) {
         window.user_p5.redraw()
         btn.disabled = false;
     });
-    container.appendChild(btn)
+    console.log(animation)
+    if (animation != null) {
+        const animationName = `button-animation-${Math.random()}`.replace(".", "");
+        const styleSheet = document.styleSheets[0];
     
+        styleSheet.insertRule(
+            `@keyframes ${animationName} { ${animation} }`,
+            styleSheet.cssRules.length
+        );
+        btn.style.animation = `${animationName} 5s ease-in-out forwards`;
+    }
+    container.appendChild(btn);
+
+
 }
