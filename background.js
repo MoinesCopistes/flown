7f2a6bc724aa75826d4eb8f29dd8c10ec659722f
kiwi <kiwi.bp.surviv@gmail.com>
2024-11-23 23:21:41 +0100

    slower background

--- background.js
+++ background.js
@@ -58,7 +58,7 @@ var canvasDiv = document.getElementById('particle-canvas');
 var options = {
   particleColor: '#888',
   interactive: true,
-  speed: 'medium',
+  speed: 'slow',
   density: 'high'
 };
 var particleCanvas = new ParticleNetwork(canvasDiv, options);
