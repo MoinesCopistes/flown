7eb046c6d758fb11d50b3c0a244613f598acbd0a
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2024-11-24 13:30:54 +0100

    test

--- src/sound_effects.js
+++ src/sound_effects.js
@@ -1,4 +1,4 @@
-export let clk_sound = new Audio("assets/sound/click.wav");
-export let win_sound = new Audio("assets/sound/win.wav"); 
-export let close_sound = new Audio("assets/sound/close.wav");
-export let end_sound = new Audio("assets/sound/end.wav");
+export let clk_sound = new Audio("click.wav");
+export let win_sound = new Audio("win.wav"); 
+export let close_sound = new Audio("close.wav");
+export let end_sound = new Audio("end.wav");
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  publicDir: 'public', // This is the default
   // Other configurations...
   base: '/flown/', // Replace 'repo-name' with your actual repository name
 });
