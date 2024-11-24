201f9395caa0232009b13098ddab3588c239bb54
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2024-11-24 12:54:10 +0100

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Other configurations...
+  base: '/flow/', // Replace 'repo-name' with your actual repository name
+});
